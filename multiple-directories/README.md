c67f65ce188d23e402e8a1ea8621c0538228110a
kaito2 <kaito2@kaito2noMacBook-Pro.local>
2022-07-30 17:19:42 +0900

    Impl

---
+++ multiple-directories/README.md
@@ -0,0 +1,13 @@
+# Multiple Directories
+
+## Gazelle
+
+```
+bazel run //:gazelle
+```
+
+## Run
+
+```
+bazel run //app1/cmd:cmd
+```
--- multiple-directories/app1/cmd/main.go
+++ multiple-directories/app1/cmd/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/kaito2/go-bazel-sample/multiple-directories/lib/example"
+)
 
 func main() {
-	fmt.Println("Hello gazelle from app1")
+	c := example.Client{}
+	fmt.Println(c.Message())
 }
